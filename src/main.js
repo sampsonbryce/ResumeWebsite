a246f3d7409bb91007837af56dab155cb7120a5d
Bryce Sampson <sampson.bryce@yahoo.com>
2018-04-10 18:04:12 +0200

    removed homemade mediaqueries

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import 'vue-awesome/icons';
 import Icon from 'vue-awesome/components/Icon';
 import VueScrollTo from 'vue-scrollto';
 import Toast from './plugins/Toast';
-import MediaQueries from './plugins/Media';
 
 Vue.use(Toast);
-Vue.use(MediaQueries);
 Vue.use(VueScrollTo);
 
 Vue.component('icon', Icon)
